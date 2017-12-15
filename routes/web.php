b2647e48ffd503f206ec633fe3262bbdf0c9cf31
arunprakasanakv <arunprakasanakv@gmail.com>
2017-12-15 16:51:16 +0530

    adminupdate

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
 Route::get('/', function () {
-    return view('user/blog');
+    return view('user.blog');
 });
 
 Route::get('post', function () {
-    return view('user/post');
+    return view('user.post');
 })->name('post');
+
+Route::get('admin/home', function () {
+    return view('admin.home');
+});
